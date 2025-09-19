19a49023677fa313b760ae19041b6be441551fef
TanayHingane <tanayhingane03@gmail.com>
2025-09-19 18:42:42 +0530

    Update route.ts

--- app/image-sitemap/route.ts
+++ app/image-sitemap/route.ts
@@ -1,45 +1,39 @@
-import { MetadataRoute } from "next";
-
-const images: string[] = [
-  "D:\\Phoenix\\public\\clicks\\c1.jpg",
-  "D:\\Phoenix\\public\\clicks\\c10.jpg",
-  "D:\\Phoenix\\public\\clicks\\c2.jpg",
-  "D:\\Phoenix\\public\\clicks\\c5.jpg",
-  "D:\\Phoenix\\public\\clicks\\c8.jpg",
-  "D:\\Phoenix\\public\\clicks\\c9.jpg",
-  "D:\\Phoenix\\public\\portfolio.png",
-  "D:\\Phoenix\\public\\safebeam03.png",
-  "D:\\Phoenix\\public\\smartbalance.png",
-  "D:\\Phoenix\\public\\snake.png",
-  "D:\\Phoenix\\public\\snakess.png",
-  "D:\\Phoenix\\public\\snk.png",
-  "D:\\Phoenix\\public\\tpp.png",
-  "D:\\Phoenix\\public\\tppp.jpg",
-];
-
 export async function GET() {
   const baseUrl = "https://tanayhingane03.vercel.app";
+  const images: string[] = [
+    "/clicks/c1.jpg",
+    "/clicks/c10.jpg",
+    "/clicks/c2.jpg",
+    "/clicks/c5.jpg",
+    "/clicks/c8.jpg",
+    "/clicks/c9.jpg",
+    "/portfolio.png",
+    "/safebeam03.png",
+    "/smartbalance.png",
+    "/snake.png",
+    "/snakess.png",
+    "/snk.png",
+    "/tpp.png",
+    "/tppp.jpg",
+  ];
+
   const imageSitemap = images
-    .map((image) => {
-      const imageUrl = image.replace("D:\\Phoenix\\public", baseUrl);
-      return `
-      <url>
-        <loc>${baseUrl}</loc>
-        <image:image>
-          <image:loc>${imageUrl}</image:loc>
-        </image:image>
-      </url>
-    `;
-    })
+    .map(
+      (image) => `
+  <url>
+    <loc>${baseUrl}</loc>
+    <image:image>
+      <image:loc>${baseUrl}${image}</image:loc>
+    </image:image>
+  </url>`
+    )
     .join("");
 
-  const sitemap = `
-    <?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-            xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
-      ${imageSitemap}
-    </urlset>
-  `;
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+  ${imageSitemap}
+</urlset>`;
 
   return new Response(sitemap, {
     headers: {
